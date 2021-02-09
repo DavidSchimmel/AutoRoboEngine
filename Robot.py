2895d8521b2371520c714522bc2594815deba6d1
David Schimmel <david.schimmel@gmx.de>
2021-02-10 00:23:35 +0100

    initial commit

---
+++ Robot.py
@@ -0,0 +1,12 @@
+class Robo:
+    def __init__():
+        velocity_left = 0
+        velocity_right = 0
+        orientation = 0
+        position = [0, 0]
+
+    def update_position():
+        return
+
+    def update_orientation():
+        return
