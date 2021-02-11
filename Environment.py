ac4ddae8d62c2b76fd65e3eeaca02f3a27bf217f
David Schimmel <david.schimmel@gmx.de>
2021-02-11 22:43:11 +0100

    Add basic robot body

--- Environment.py
+++ Environment.py
@@ -1,25 +1,50 @@
 import sys, pygame
+from pygame.draw import circle
+from Config import Config
+import Constants as C
+from Robot import Robot
+
+config = Config()
+
 pygame.init()
 
-size = width, height = 1024, 760
+size = width, height = 1024, 768
 speed = [2, 2]
-black = 0, 0, 0
 
 screen = pygame.display.set_mode(size)
 
-ball = pygame.image.load("intro_ball.gif")
-ballrect = ball.get_rect()
+robot = Robot(config, pygame, screen, (50,50,100), [100, 700], [0, -1] , 25)
+
+
+"""ball = pygame.image.load("intro_ball.gif")
+ballrect = ball.get_rect()"""
+
+def render(robot):
+    robot.draw()
 
 while 1:
     for event in pygame.event.get():
         if event.type == pygame.QUIT: sys.exit()
 
-    ballrect = ballrect.move(speed)
+        if event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_w:
+                robot.acellarate("left")
+            elif event.key == pygame.K_k:
+                robot.acellarate("right")
+
+    robot.update_position()
+
+
+    """ballrect = ballrect.move(speed)
     if ballrect.left < 0 or ballrect.right > width:
         speed[0] = -speed[0]
     if ballrect.top < 0 or ballrect.bottom > height:
         speed[1] = -speed[1]
 
-    screen.fill(black)
-    screen.blit(ball, ballrect)
+    screen.fill(C.BLACK)
+    screen.blit(ball, ballrect)"""
+
+    # update()
+    render(robot)
+
     pygame.display.flip()
--- Robot.py
+++ Robot.py
@@ -1,12 +1,42 @@
-class Robo:
-    def __init__():
-        velocity_left = 0
-        velocity_right = 0
-        orientation = 0
-        position = [0, 0]
+from pygame.draw import (circle, aaline)
 
-    def update_position():
+class Robot:
+    def __init__(self, config, pygame, display, colour, position, orientation = (0, -1), size = 10):
+        self.config         = config
+
+        self.game           = pygame
+        self.display        = display
+
+        self.velocity_left  = 0
+        self.velocity_right = 0
+        self.orientation    = orientation
+        self.position       = position
+        self.colour         = colour
+        self.size           = size
+
+        self.draw_explicit(self.game, self.display, self.colour, self.position, self.size, self.orientation)
+
+    def draw_explicit(self, pygame, display, colour, position, size, orientation):
+        # draw robot body as circle
+        pygame.draw.circle(display, colour, position, size)
+
+        # draw direction indicator
+        face_position = (position[0] + orientation[0] * size, position[1] + orientation[1] * size)
+        pygame.draw.aaline(display, (123, 12, 12), position, face_position)
         return
 
-    def update_orientation():
+    def draw(self):
+        self.draw_explicit(self.game, self.display, self.colour, self.position, self.size, self.orientation)
+
+    def update_position(self):
+        self.position[1] = self.position[1] + self.velocity_left + self.velocity_right
         return
+
+    def update_orientation(self):
+        return
+
+    def acellarate(self, side):
+        if side   == "left":
+            self.velocity_left  += self.config.ACCELERATION * -1 # FIXME
+        elif side == "right":
+            self.velocity_right += self.config.ACCELERATION * -1 # FIXME
