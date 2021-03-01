8dced61d9802ec42b736bb6cc23cdc2c6516e84d
David Schimmel <david.schimmel@gmx.de>
2021-03-01 17:17:59 +0100

    add visualisation and elitism

--- evolution_benchmark.py
+++ evolution_benchmark.py
@@ -5,14 +5,14 @@ import math
 class evolution_benchmark():
 
     def __init__(self, F, bounds, num_pop, num_gen):
-        self.F = F #Fit function NB. Best fit = Higher fitnes value, so to find minimum put the - before! 
+        self.F = F #Fit function NB. Best fit = Higher fitnes value, so to find minimum put the - before!
         self.bounds = np.array(bounds) #Function bounds
         self.num_pop = num_pop #Number of individuals in population
         self.fit = np.zeros(num_pop) #evaluation/fit for each indivial
         self.num_gen = num_gen #Number of generations
-        self.mutation_rate = 0.5 #Probability that a mutation occour, Xover and Mutation have the same probability
-        self.n_best = 3 #Number of best individuals allowed to reproduce (where required)
-        self.mutation_coef = 0.1 #Magnitude of the mutation
+        self.mutation_rate = 0.95 #Probability that a mutation occour, Xover and Mutation have the same probability
+        self.n_best = math.floor(num_pop*0.75) #Number of best individuals allowed to reproduce (where required)
+        self.mutation_coef = 0.01 #Magnitude of the mutation
 
     def _create_individual(self, mode='uni_sampled'):
         if mode == 'std_sampled':
@@ -20,7 +20,7 @@ class evolution_benchmark():
         elif mode == 'uni_sampled':
             genotype=np.random.uniform(size=(2))
         return np.multiply(genotype, self.bounds[:,1]-self.bounds[:,0])+self.bounds[:,0]
-        
+
     def initialization(self, mode='uni_sampled'):
         self.population = []
         for _ in range(self.num_pop):
@@ -44,8 +44,19 @@ class evolution_benchmark():
                     new_population.append(self.population[i].copy())
                     if len(new_population)>=self.num_pop: break
             return new_population
-        elif mode == 'a':
-            pass
+        elif mode == 'elitism':
+            for i in range(n_best):
+                new_population.append(self.population[i].copy())
+            k = 0
+            for i in range(n_best,len(self.population)):
+                if (k >= n_best):
+                    k = 0
+                if (random.random() > 0.7):
+                    new_population.append(self.population[k].copy())
+                    k += 1
+                else:
+                    new_population.append(self.population[i].copy())
+            return new_population
 
     def Xover(self, p1, p2, mode=0):
         if mode == 0: #arithmetic
@@ -69,11 +80,22 @@ class evolution_benchmark():
         return p
 
     def evolution(self, mantain_best=True):
+        ##########################
+        positions = [[],[]]
+        ##########################
         self.initialization()
         for g in range(self.num_gen):
             self.evaluation()
+            #########################
+            positions[0].append([])
+            positions[1].append([])
+
+            for individual in self.population:
+                positions[0][g].append(individual[0])
+                positions[1][g].append(individual[1])
+            #########################
             print('Generation ',g,' Best: ',self.population[0],' with value: ', self.fit[0])
-            self.population = self.selection_reproduction(n_best=self.n_best)
+            self.population = self.selection_reproduction(mode='elitism', n_best=self.n_best)
             start = 0 if not mantain_best else self.n_best
             for p in range(start, self.num_pop):
                 if random.random()>self.mutation_rate:
@@ -81,6 +103,9 @@ class evolution_benchmark():
                         self.population[p] = self.Xover(self.population[p], self.population[random.randint(0, self.num_pop-1)], mode=random.randint(0, 2))
                     else:
                         self.population[p] = self.mutation(self.population[p])
+
+        #######################
+        return positions
 
 def benchmark_1(p):
     #Rosenbrock
@@ -99,5 +124,64 @@ def benchmark_2(p):
     f = pow(x, 2) - 10 * math.cos(2 * math.pi * x) + pow(y, 2) - 10 * math.cos(2 * math.pi * y) + 10*n
     return  -f
 
-eb = evolution_benchmark(benchmark_1, [[0,10],[0,10]], 10, 10)
-eb.evolution()
+
+##################################################################
+
+
+
+eb = evolution_benchmark(benchmark_2, [[-10,10],[-10,10]], 50, 30)
+positions = eb.evolution()
+
+##################################################################
+
+import matplotlib
+import matplotlib.pyplot as plt
+import matplotlib.animation as animation
+import pandas as pd
+from random import random
+import seaborn as sns
+
+AXIS_X_LFT = -10
+AXIS_X_RGT =  10
+AXIS_Y_BOT = -10
+AXIS_Y_TOP =  10
+
+
+Writer = animation.writers['ffmpeg']
+writer = Writer(fps=8, metadata=dict(artist='Me'), bitrate=1800)
+#Init of the function graph
+fig = plt.figure(figsize=(10,6))
+plt.xlim(AXIS_X_LFT, AXIS_X_RGT)
+plt.ylim(AXIS_Y_BOT, AXIS_Y_TOP)
+plt.xlabel('X')
+plt.ylabel('Y')
+plt.title('plot')
+#Benchmark functions
+a = 0; b = 1
+x = np.arange(AXIS_X_LFT, AXIS_X_RGT, 0.1)
+y = np.arange(AXIS_Y_BOT, AXIS_Y_TOP, 0.1)
+xx, yy = np.meshgrid(x, y)
+
+#z = ((a-(xx**2))**2) + b * ((yy - (xx**2))**2) #Rosenbrock
+z = 10 * 2 + ((xx**2) - 10 * np.cos(2 * math.pi * xx)) + ((yy**2) - 10 * np.cos(2 * math.pi * yy)) #Rastrigin
+
+df1 = pd.DataFrame(data=z, index=y, columns=x)
+df1.style.background_gradient(cmap ='inferno').set_properties(**{'font-size': '20px'})
+h = plt.contourf(x,y,z, levels=1000)
+
+#Business logic
+#particels=[]
+#positions=[[],[]]
+#f,pos=search_min(N_PARTICLES, 1, 0.0033, 2, 2, 1.4, 100, 0.001, Rosenbrock, [[AXIS_X_LFT,AXIS_X_RGT],[AXIS_Y_BOT,AXIS_Y_TOP]], animation=True)
+
+positions=np.array(positions)
+#print('Value: ', f, 'Position: ', pos)
+ln, = plt.plot([], [], 'ro')
+def update(f): #Frame's update
+    print(f)
+    ln.set_data(positions[0][f], positions[1][f])
+    return ln,
+
+ani = matplotlib.animation.FuncAnimation(fig, update, frames=positions.shape[1], interval = 1, blit = False) #to print all the frames frames=positions.shape[1]
+ani.save('testrun.mp4', writer=writer)
+plt.show()
